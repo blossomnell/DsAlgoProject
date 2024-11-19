291705b64a414313137748350c6477754649b29c
blossomnell <blossom83@gmail.com>
2024-11-19 17:52:19 -0600

    Update CucumberTest.java

--- src/test/java/testRunner/CucumberTest.java
+++ src/test/java/testRunner/CucumberTest.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features", //
 		glue = { "stepDefinitions", "appHooks" }, //
-		plugin = { "pretty", "html:target/cucumber-reports.html" }, //
+		plugin = { "pretty", "html:target/cucumber-reports.html", "json:target/cucumber.json" }, 
 		dryRun = false, //
 		tags = "@tag"//
 
