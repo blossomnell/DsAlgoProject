9610e0f1f50cee0c47a6d6c21357f17b639b9255
blossomnell <blossom83@gmail.com>
2024-11-19 17:52:51 -0600

    Update testRunner.java

--- src/test/java/testRunner/testRunner.java
+++ src/test/java/testRunner/testRunner.java
@@ -5,10 +5,11 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/resources/features", glue = { "stepDefinitions", "appHooks" }, plugin = {
-		"pretty", "html:target/cucumber-reports.html", "json:target/cucumber.json" },
+@CucumberOptions(features = "src/test/resources/features", glue = { "stepDefinitions", "appHooks" }, 
+		 plugin = {"pretty", "html:target/cucumber-reports.html", "json:target/cucumber.json" },
 		// dryRun = false,
-		monochrome = true, tags = "@tag")
+		monochrome = true, 
+		 tags = "@tag")
 
 public class testRunner extends AbstractTestNGCucumberTests {
 
