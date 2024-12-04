8a2cd90bdb52d8ae7c5fd0fb59e4b452f0681852
blossomnell <blossom83@gmail.com>
2024-12-04 11:36:10 -0600

    testrunner changes

--- src/test/java/testRunner/CucumberTest.java
+++ src/test/java/testRunner/CucumberTest.java
@@ -9,7 +9,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(features = "src/test/resources/features", //
 		glue = { "stepDefinitions", "appHooks" }, //
 		plugin = { "pretty", "html:target/cucumber-reports.html", "json:target/cucumber.json", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:", },
-		tags = "@smoke"
+		tags = "@tag"
 		
 )
 public class CucumberTest extends AbstractTestNGCucumberTests {
