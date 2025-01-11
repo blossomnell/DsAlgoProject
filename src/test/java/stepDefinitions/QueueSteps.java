73c14b30e5f949a1d8e60a3f388ec894baee309c
Sowmyapenmetsa <psowmyavarma77@gmail.com>
2025-01-11 14:28:05 -0600

    removed all extra spaces

--- src/test/java/stepDefinitions/QueueSteps.java
+++ src/test/java/stepDefinitions/QueueSteps.java
@@ -14,22 +14,14 @@ import pageObjects.QueuePage;
 import pageObjects.StackPage;
 
 public class QueueSteps {
-	
-	 ExcelReader excelReader;
-	    public QueueSteps()  {
-	        try {
 
-	        	
-//	            String filePath = System.getProperty("user.dir") + "/src/test/resources/config/TestData.xlsx";
-	        	   String filePath = System.getProperty("user.dir") + "/" + reader.init_prop().getProperty("excelFilePath");
-	            excelReader = new ExcelReader(filePath);
-	        } catch (IOException e) {
-	            throw new RuntimeException("Failed to load TestData.xlsx file: " + e.getMessage(), e);
-	        }
-	    }
-	    
-	    configReader reader = new configReader();
-	QueuePage queue= new QueuePage();
+	public QueueSteps() {
+
+	}
+
+	configReader reader = new configReader();
+	QueuePage queue = new QueuePage();
+
 	@When("the user clicks on the Get started button under queue")
 	public void the_user_clicks_on_the_get_started_button_under_queue() {
 		queue.clickGetStarted();
@@ -37,8 +29,8 @@ public class QueueSteps {
 
 	@Then("the user navigates to the queue page")
 	public void the_user_navigates_to_the_queue_page() {
-		Assert.assertTrue( queue.isQueuePageDisplayed(),"Queue page is not displayed");
-	    
+		Assert.assertTrue(queue.isQueuePageDisplayed(), "Queue page is not displayed");
+
 	}
 
 	@Given("the user in th queue page")
@@ -49,39 +41,42 @@ public class QueueSteps {
 
 	@When("the user clicks on the Implementation of Queue in Python link")
 	public void the_user_clicks_on_the_implementation_of_queue_in_python_link() {
-		 queue.clickImplementationOfQueueInPythonLink();
+		queue.clickImplementationOfQueueInPythonLink();
 	}
 
 	@Then("the user navigates to the Implementation of Queue in Python page")
 	public void the_user_navigates_to_the_implementation_of_queue_in_python_page() {
-		 Assert.assertTrue(queue.isImplementationOfQueueInPythonPageDisplayed(),"Implementation of queue in python page is not displayed");
-		    
+		Assert.assertTrue(queue.isImplementationOfQueueInPythonPageDisplayed(),
+				"Implementation of queue in python page is not displayed");
+
 	}
 
 	@Given("the user in Implementation of Queue in Python page")
 	public void the_user_in_implementation_of_queue_in_python_page() {
 //	   queue.navigateToImplementationOfQueueInPython();
 		queue.clickGetStarted();
-		 queue.clickImplementationOfQueueInPythonLink();
+		queue.clickImplementationOfQueueInPythonLink();
 	}
 
 	@Given("the user is in the try editor page in Implementation of Queue in Python")
 	public void the_user_is_in_the_try_editor_page_in_implementation_of_queue_in_python() {
 //		queue.navigateToTryEditorPage();
 		queue.clickGetStarted();
-		 queue.clickImplementationOfQueueInPythonLink();
+		queue.clickImplementationOfQueueInPythonLink();
 		queue.clickTryhereButton();
-		
+
 	}
+
 	@When("the user clicks on the Implementation using collections.deque link")
 	public void the_user_clicks_on_the_implementation_using_collections_deque_link() {
-	    queue.clickImplementationUsingCollectionsDequeLink();
+		queue.clickImplementationUsingCollectionsDequeLink();
 	}
 
 	@Then("the user navigates to the Implementation using collections.deque page")
 	public void the_user_navigates_to_the_implementation_using_collections_deque_page() {
-		
-		Assert.assertTrue(queue.isImplementationUsingCollectionDequePageDispayed(), "implementation using collections deque page is not displayed");
+
+		Assert.assertTrue(queue.isImplementationUsingCollectionDequePageDispayed(),
+				"implementation using collections deque page is not displayed");
 	}
 
 	@Given("the user in Implementation using collections.deque page")
@@ -98,25 +93,25 @@ public class QueueSteps {
 		queue.clickImplementationUsingCollectionsDequeLink();
 		queue.clickTryhereButton();
 	}
-	
-	
-	
+
 	@When("the user clicks on the Implementation using array link")
 	public void the_user_clicks_on_the_implementation_using_array_link() {
 		queue.clickImplementationUsingarrayLink();
-	    
+
 	}
 
 	@Then("the user navigates to the Implementation using array page")
 	public void the_user_navigates_to_the_implementation_using_array_page() {
-		Assert.assertTrue(queue.isImplementationUsingArrayPageDispayed(), "implementation using array page is not displayed");	}
+		Assert.assertTrue(queue.isImplementationUsingArrayPageDispayed(),
+				"implementation using array page is not displayed");
+	}
 
 	@Given("the user in Implementation using array page")
 	public void the_user_in_implementation_using_array_page() {
 //		queue.navigateToImplementationUsingArrayPage();	
 		queue.clickGetStarted();
 		queue.clickImplementationUsingarrayLink();
-		}
+	}
 
 	@Given("the user is in the try editor page in Implementation using array")
 	public void the_user_is_in_the_try_editor_page_in_implementation_using_array() {
@@ -125,10 +120,7 @@ public class QueueSteps {
 		queue.clickImplementationUsingarrayLink();
 		queue.clickTryhereButton();
 	}
-	
-	
-	
-	
+
 	@When("the user clicks on the Queue Operations link")
 	public void the_user_clicks_on_the_queue_operations_link() {
 		queue.clickQueueOperationsLink();
@@ -136,9 +128,10 @@ public class QueueSteps {
 
 	@Then("the user navigates to the Queue Operations page")
 	public void the_user_navigates_to_the_queue_operations_page() {
-		Assert.assertTrue(queue.isQueueOperationsPageDispayed(), "Queue operations page is not displayed");	
+		Assert.assertTrue(queue.isQueueOperationsPageDispayed(), "Queue operations page is not displayed");
 
 	}
+
 	@Given("the user in operations in queue page")
 	public void the_user_in_operations_in_queue_page() {
 //		queue.operationsInQueuePage();
@@ -146,6 +139,7 @@ public class QueueSteps {
 		queue.clickQueueOperationsLink();
 
 	}
+
 	@Given("the user is in the try editor page in Queue Operations")
 	public void the_user_is_in_the_try_editor_page_in_queue_operations() {
 //		queue.navigateToTryEditorPage();
@@ -155,38 +149,11 @@ public class QueueSteps {
 
 	}
 
-
 	@Given("the user in Queue Operations page")
 	public void the_user_in_queue_operations_page() {
 //		queue.operationsInQueuePage();
 		queue.clickGetStarted();
 		queue.clickQueueOperationsLink();
 	}
-	
-//	@When("the user clicks on the Practice questions link")
-//	public void the_user_clicks_on_the_practice_questions_link() {
-//		queue.clickPracticeQuestionsLink();
-//	    
-//	}
-//
-//	@Then("the user navigates to the Practice page")
-//	public void the_user_navigates_to_the_practice_page() {
-//		Assert.assertTrue(queue.isPracticePageDisplayed(),"Practice page is not Displayed");
-//	    
-//	}
-
-	
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
